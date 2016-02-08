9a9c79fdf755d71302ab466ff96b243eb08db0c2
admin <gerguno@gmail.com>
2016-02-08 16:28:38 +0200

    edited analytics

--- analytics/analytics.js
+++ analytics/analytics.js
@@ -2,21 +2,12 @@ var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-73434776-1']);
 _gaq.push(['_trackPageview']);
 
-function trackButton(e) {
-	_gaq.push(['_trackEvent', e.target.id, 'clicked']);
-};
-
-var buttons = document.querySelectorAll('a'),
-	switcher = document.querySelector('#switcher');
-for (var i = 0; i < buttons.length; i++) {
-	buttons[i].addEventListener('click', trackButtonClick);
-}
-switcher.addEventListener('click', trackButtonClick);
-
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = 'https://ssl.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+
+
 
 
