a3e05bdc0dda117b3d6a4129d532d16fd8184c41
admin <gerguno@gmail.com>
2016-02-11 16:51:44 +0200

    updated analytics

--- analytics/analytics.js
+++ analytics/analytics.js
@@ -1,3 +1,4 @@
+// var 1
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-73434776-1']);
 _gaq.push(['_trackPageview']);
@@ -11,12 +12,41 @@ _gaq.push(['_trackPageview']);
 
 function trackButton(e) {
   _gaq.push(['_trackEvent', e.target.id, 'clicked']);
+  console.log('send');
 };
 
-var buttons = document.querySelectorAll('a'),
-    switcher = document.querySelector('#switcher');
+var buttons = document.querySelectorAll('a');
 for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', trackButtonClick);
+    buttons[i].addEventListener('click', trackButton(this));
 }
-switcher.addEventListener('click', trackButtonClick);
 
+
+
+
+
+// // var 2
+//   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+//   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+//   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+//   })(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
+
+//   ga('create', 'UA-73434776-1', 'auto');
+//   ga('send', 'pageview');
+
+//   ga(function(tracker) {
+// 	  // Logs the tracker created above to the console.
+// 	  console.log(tracker);
+// 	  console.log(ga.getAll());
+//   });
+
+
+// function trackButton(e) {
+//   ga('send', 'event', e.target.id, 'clicked');
+// };
+
+// var buttons = document.querySelectorAll('a'),
+//     switcher = document.querySelector('#switcher');
+// for (var i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener('click', trackButton(this));
+// }
+// switcher.addEventListener('click', trackButton(this));
